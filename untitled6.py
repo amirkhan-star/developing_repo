777ca96a4399afc595f19d1f5481002981b23f12
Amirkhan <musayev.emirxan13@gmail.com>
2024-07-28 09:33:15 +0000

    asdadslmksmdksdssds5d15s1d

---
+++ untitled6.py
@@ -0,0 +1,6 @@
+#please enter your credentials
+MINIO_user_name = 5#Enter minIO username
+MINIO_password = 6#Enter minIO password 
+bucket_names = 8#Enter your bucket which you want to arcive files
+a = MINIO_user_name+MINIO_password+bucket_names
+print(a)
