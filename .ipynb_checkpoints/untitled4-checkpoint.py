86f9e275996e843355d11c476fa71dfb92de6f00
Amirkhan <musayev.emirxan13@gmail.com>
2024-07-28 09:21:59 +0000

    asdadslmsmkdmkxcmkmas

---
+++ .ipynb_checkpoints/untitled4-checkpoint.py
@@ -0,0 +1,5 @@
+#please enter your credentials
+MINIO_user_name = #Enter minIO username
+MINIO_password = #Enter minIO password 
+bucket_names = #Enter your bucket which you want to arcive files
+print('sadad')
