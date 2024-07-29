6562fd9e2c378a520db38c26a4230ec0756c54e4
Amirkhan <musayev.emirxan13@gmail.com>
2024-07-29 06:58:35 +0000

    Adnjsadma

---
+++ untitled55.py
@@ -0,0 +1,30 @@
+#please enter your credentials
+MINIO_user_name = 'AMIRKHAN'
+MINIO_password = 'amirkhan1321'
+bucket_names = 'newbucket'
+#for using minio server execution
+local_dir = '/home/ubuntu/Desktop/dirforminIO'
+
+import os
+import pandas as pd 
+import xml.etree.ElementTree as ET
+
+def xml_to_excel(xml_path, excel_path):
+    tree = ET.parse(xml_path)
+    root = tree.getroot()
+    data = []
+    
+    for item in root.findall('.//item'): 
+        row = {child.tag: child.text for child in item}
+        data.append(row)
+    df = pd.DataFrame(data)
+    df.to_excel(excel_path, index=False)
+    
+def convert_all_xml_in_directory(directory_path):
+    for filename in os.listdir(directory_path):
+        if filename.endswith('.xml'):
+            xml_path = os.path.join(directory_path, filename)
+            excel_path = os.path.join(directory_path, filename.replace('.xml', '.xlsx'))
+            xml_to_excel(xml_path, excel_path)
+            os.remove(xml_path)
+convert_all_xml_in_directory(local_dir)
